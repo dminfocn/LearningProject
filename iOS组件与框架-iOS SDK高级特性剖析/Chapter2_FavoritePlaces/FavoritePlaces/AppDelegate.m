e4bbc49ae06c91dddd25deadfd336e425f14e7ba
zhanlong <zhzhl1993@163.com>
2017-10-26 23:24:00 +0800

    完善CoreData

--- iOS组件与框架-iOS SDK高级特性剖析/Chapter2_FavoritePlaces/FavoritePlaces/AppDelegate.m
+++ iOS组件与框架-iOS SDK高级特性剖析/Chapter2_FavoritePlaces/FavoritePlaces/AppDelegate.m
@@ -30,25 +30,22 @@
     return YES;
 }
 
-
 - (void)applicationWillResignActive:(UIApplication *)application {
     ZLLocationManager *appLocationManager = [ZLLocationManager sharedLocationManager];
     [appLocationManager.locationManager stopUpdatingLocation];
 }
 
-//- (void)saveContext
-//{
-//    NSError *error = nil;
-//    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-//    if (managedObjectContext != nil) {
-//        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-//            abort();
-//        }
-//    }
-//}
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
 
 #pragma mark - Core Data stack
 // Returns the managed object model for the application.
